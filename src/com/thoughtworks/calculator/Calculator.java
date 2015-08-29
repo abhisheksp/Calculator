45f4a10566695a60315f47bf17b0a74eed5b2849
nikhilsu <nikhilsu@thoughtworks.com>
2015-08-29 16:23:51 +0530

    Added a Spec for calculator, should return zero initially when an invalid command is input

---
+++ src/com/thoughtworks/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.calculator;
+//calculator has a result on which calculations are performed, based on a input
+
+public class Calculator {
+
+    public double resultOf(String command) {
+        return 0.0;
+    }
+}
