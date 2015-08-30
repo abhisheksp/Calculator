15bf064f602407bb14f47a3a1df566d1733dcece
nikhilsu <nikhilsu@thoughtworks.com>
2015-08-31 00:06:53 +0530

    Added Spec to calculator class for it should return the negative of the number that was passed with the subtract command after an invalid command was issued initially

--- test/com/thoughtworks/calculator/CalculatorTest.java
+++ test/com/thoughtworks/calculator/CalculatorTest.java
@@ -56,4 +56,13 @@ public class CalculatorTest {
 
         assertEquals(5.0, calculator.resultOf("Add 5"), 0.0);
     }
+
+    @Test
+    public void shouldReturnTheNegativeOfTheNumberPassedWithTheSubtractCommandAfterAnInvalidCommandWasIssuedInitially() {
+        Calculator calculator = new Calculator();
+
+        calculator.resultOf(" ");
+
+        assertEquals(-5.0, calculator.resultOf("Subtract 5"), 0.0);
+    }
 }
