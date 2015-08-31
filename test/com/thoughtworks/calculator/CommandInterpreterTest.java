72f8e0c6dcd38b040678549b2bbb7b2b85a1289d
nikhilsu <nikhilsu@thoughtworks.com>
2015-08-31 22:21:13 +0530

    Added Spec for the CommandInterpreter class should retru nthe product of the accumulator and the number passed with the multiply command

--- test/com/thoughtworks/calculator/CommandInterpreterTest.java
+++ test/com/thoughtworks/calculator/CommandInterpreterTest.java
@@ -33,4 +33,18 @@ public class CommandInterpreterTest {
 
         assertEquals(5.0, commandInterpreter.interpret("subtract 5.0"), 0.0);
     }
+
+    @Test
+    public void shouldReturnZeroWhenMultiplyCommandIssuedInitially() {
+        CommandInterpreter commandInterpreter = new CommandInterpreter(0.0);
+
+        assertEquals(0.0, commandInterpreter.interpret("multiply 5.0"), 0.0);
+    }
+
+    @Test
+    public void shouldReturnTheProductOfAccumulatorAndTheNumberPassedWithTheMultiplyCommand() {
+        CommandInterpreter commandInterpreter = new CommandInterpreter(10.0);
+
+        assertEquals(50.0, commandInterpreter.interpret("multiply 5.0"), 0.0);
+    }
 }
